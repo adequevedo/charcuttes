96b1e9a57dfce6e5b11d0651f832763c45ab6067
Alex deQuevedo <alexdequevedo@gmail.com>
2022-03-14 19:55:09 -0400

    initial commit, setup components and file structure

---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './css/home.css'
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+      <h1>Figs and Jellies</h1>
+      </div>
+    );
+  }
+}
+
+
+export default Home;
---
+++ src/Team.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './css/team.css'
+
+class Team extends Component {
+  render() {
+    return (
+      <div className="Team">
+      <h1>Team</h1>
+      </div>
+    );
+  }
+}
+
+
+export default Team;
